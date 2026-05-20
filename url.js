a2e0f9bdc7d45410672c058b876509fca768d6a8
katelyn0725kimo-ctrl <katelyn0725kimo@gmail.com>
2026-05-20 16:33:00 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,7 +8,7 @@
 
 
 const urls = [
-	'https://hhming.moe',
-	'https://github.com/hms5232',
-	'https://gitlab.com/hms5232',
+	'https://www.surveycake.com/s/ewqrq',
+	'https://www.surveycake.com/s/9Mexr',
+	'https://www.surveycake.com/s/P3XDG',
 ];
